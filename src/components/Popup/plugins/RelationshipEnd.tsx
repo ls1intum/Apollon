33a8b131babaa5c12bf7c2d4f45e3871a090c656
Julian Willand <willand@in.tum.de>
2019-03-04 10:42:35 +0100

    Implemented switch to change source and target location of relationships.

--- src/components/Popup/plugins/RelationshipEnd.tsx
+++ src/components/Popup/plugins/RelationshipEnd.tsx
@@ -1,30 +1,79 @@
 import React, { SFC } from 'react';
 import { connect } from 'react-redux';
 import Relationship from './../../../domain/Relationship';
-import { ElementRepository } from '../../../domain/Element';
+import Element, { ElementRepository } from '../../../domain/Element';
+import { State } from '../../Store';
 
-const RelationshipEnd: SFC<Props> = ({ relationship, end, update }) => {
+const RelationshipEnd: SFC<Props> = ({
+  relationship,
+  end,
+  elements,
+  getById,
+  update,
+}) => {
   const multiplicity: string = (relationship as any)[`${end}Multiplicity`];
   const role: string = (relationship as any)[`${end}Role`];
+  const element = getById(relationship[end].element);
+  const targets = elements.filter(e => (e.constructor as any).isConnectable);
 
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const rel = {
+  const onChangeSelect = (event: React.FormEvent<HTMLSelectElement>) => {
+    update({
+      ...relationship,
+      [end]: {
+        ...relationship[end],
+        [event.currentTarget.name]: event.currentTarget.value,
+      },
+    });
+  };
+
+  const onChangeText = (event: React.FormEvent<HTMLInputElement>) => {
+    update({
       ...relationship,
       [event.currentTarget.name]: event.currentTarget.value,
-    };
-    update(rel);
+    });
   };
 
   return (
     <table>
       <tbody>
         <tr>
+          <td>Entity:</td>
+          <td>
+            <select
+              name="element"
+              value={element.id}
+              onChange={onChangeSelect}
+            >
+              {targets.map(target => (
+                <option key={target.id} value={target.id}>
+                  {target.name}
+                </option>
+              ))}
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td>Edge:</td>
+          <td>
+            <select
+              name="location"
+              value={relationship[end].location}
+              onChange={onChangeSelect}
+            >
+              <option value="N">Top</option>
+              <option value="W">Left</option>
+              <option value="E">Right</option>
+              <option value="S">Bottom</option>
+            </select>
+          </td>
+        </tr>
+        <tr>
           <td>Multiplicity:</td>
           <td>
             <input
               name={`${end}Multiplicity`}
               value={multiplicity}
-              onChange={onChange}
+              onChange={onChangeText}
               placeholder="0..*"
             />
           </td>
@@ -35,7 +84,7 @@ const RelationshipEnd: SFC<Props> = ({ relationship, end, update }) => {
             <input
               name={`${end}Role`}
               value={role}
-              onChange={onChange}
+              onChange={onChangeText}
               placeholder="children"
             />
           </td>
@@ -50,7 +99,10 @@ interface OwnProps {
   end: 'source' | 'target';
 }
 
-interface StateProps {}
+interface StateProps {
+  getById: (id: string) => Element;
+  elements: Element[];
+}
 
 interface DispatchProps {
   update: typeof ElementRepository.update;
@@ -58,7 +110,10 @@ interface DispatchProps {
 
 type Props = OwnProps & StateProps & DispatchProps;
 
-export default connect<StateProps, DispatchProps, OwnProps>(
-  null,
+export default connect<StateProps, DispatchProps, OwnProps, State>(
+  state => ({
+    getById: ElementRepository.getById(state.elements),
+    elements: ElementRepository.read(state),
+  }),
   { update: ElementRepository.update }
 )(RelationshipEnd);
