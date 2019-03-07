ab1a3fe1c528a49323c35f3eab0f1b3e4da32e74
Julian Willand <willand@in.tum.de>
2019-03-07 09:59:55 +0100

    WIP: started to transform scripts in example to typescript.

--- example/index.ts
+++ example/index.ts
@@ -1,3 +1,20 @@
-import ApollonEditor from './../src';
+import Apollon, { ApollonOptions } from './../src';
+import { DiagramType } from '../src/domain/Diagram';
+import { ApollonMode } from '../src/services/EditorService';
 
-(window as any).ApollonEditor = ApollonEditor;
+const container = document.getElementById('apollon')!;
+const options: ApollonOptions = {
+  initialState: JSON.parse(window.localStorage.getItem('apollon')!),
+  diagramType: DiagramType.ClassDiagram,
+  mode: ApollonMode.Full,
+  theme: {
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, HelveticaNeue, Arial, sans-serif',
+  },
+};
+const editor = new Apollon(container, options);
+
+export const onChange = (event: MouseEvent) => {
+  const { name, value } = event.target as HTMLSelectElement;
+  console.log('change', name, value);
+};
