1e5009ca990eba309c34ec66216153c5ba41d6d5
Julian Willand <willand@in.tum.de>
2019-04-03 20:54:44 +0200

    Introduced half icons.

--- src/components/layouted-element/assessable.tsx
+++ src/components/layouted-element/assessable.tsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { IAssessment } from '../../services/assessment/assessment';
 import { CancelIcon } from '../controls/cancel-icon';
 import { CheckIcon } from '../controls/check-icon';
+import { HalfcancelIcon } from '../controls/halfcancel-icon';
+import { HalfcheckIcon } from '../controls/halfcheck-icon';
 import { ModelState } from '../store/model-state';
 import { ElementComponent, OwnProps } from './element-component';
 
@@ -11,19 +13,33 @@ export const assessable = (WrappedComponent: typeof ElementComponent): Component
     render() {
       const { assessment, element } = this.props;
 
-      const count: number = (assessment && Math.min(3, Math.ceil(Math.abs(assessment.score)))) || 0;
+      const count: number = (assessment && Math.min(3, Math.floor(Math.abs(assessment.score)))) || 0;
       const component = assessment && assessment.score > 0 ? <CheckIcon fill="green" /> : <CancelIcon fill="red" />;
       const generator = count > 0 ? [...Array(count)] : [];
 
-      const icons = generator.map((_, i) => (
-        <g key={i} transform={`translate(${i * 12} 2)`}>
-          {component}
-        </g>
-      ));
+      let i = 0;
+      let icons: JSX.Element[] = [];
+      if (assessment && assessment.score % 1) {
+        icons = [
+          <g key={0} transform={`translate(0 2)`}>
+            {assessment.score > 0 ? <HalfcheckIcon fill="green" /> : <HalfcancelIcon fill="red" />}
+          </g>,
+        ];
+        i += 1;
+      }
+
+      icons = [
+        ...icons,
+        ...generator.map(_ => (
+          <g key={i + 1} transform={`translate(${i++ * 12} 2)`}>
+            {component}
+          </g>
+        )),
+      ];
 
       return (
         <WrappedComponent {...this.props}>
-          {assessment && <g transform={`translate(${element.bounds.width - (count + 1) * 12} 0)`}>{icons}</g>}
+          {assessment && <g transform={`translate(${element.bounds.width - (icons.length + 1) * 12} 0)`}>{icons}</g>}
         </WrappedComponent>
       );
     }
