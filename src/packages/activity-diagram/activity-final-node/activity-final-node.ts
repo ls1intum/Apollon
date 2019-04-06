a1ea18d5fcb61e707bb0f01be27a5568f4010b65
Julian Willand <willand@in.tum.de>
2019-04-06 18:18:46 +0200

    Fixed resize of activity diagram elements.

--- src/packages/activity-diagram/activity-final-node/activity-final-node.ts
+++ src/packages/activity-diagram/activity-final-node/activity-final-node.ts
@@ -1,10 +1,20 @@
 import { ActivityElementType } from '..';
-import { Element } from '../../../services/element/element';
-import { Boundary } from '../../../utils/geometry/boundary';
+import { Element, IElement } from '../../../services/element/element';
+import { UMLElement } from '../../../typings';
 
 export class ActivityFinalNode extends Element {
   static features = { ...Element.features, editable: false };
 
   type = ActivityElementType.ActivityFinalNode;
-  bounds: Boundary = { ...this.bounds, width: 45, height: 45 };
+
+  constructor(values?: IElement);
+  constructor(values?: UMLElement);
+  constructor(values?: IElement | UMLElement);
+  constructor(values?: IElement | UMLElement) {
+    super(values);
+
+    if (!values) {
+      Object.assign(this, { bounds: { ...this.bounds, width: 45, height: 45 } });
+    }
+  }
 }
