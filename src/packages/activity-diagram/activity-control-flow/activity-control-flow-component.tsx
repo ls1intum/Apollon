26ac95ffe62c7bfae10116aace5fabff9e2aa0c5
Julian Willand <willand@in.tum.de>
2019-04-09 18:15:58 +0200

    Center name of control flows.

--- src/packages/activity-diagram/activity-control-flow/activity-control-flow-component.tsx
+++ src/packages/activity-diagram/activity-control-flow/activity-control-flow-component.tsx
@@ -1,39 +1,41 @@
 import React, { SFC } from 'react';
-import { Port } from '../../../services/element/port';
-import { Direction } from '../../../typings';
+import { Point } from '../../../utils/geometry/point';
 import { ActivityControlFlow } from './activity-control-flow';
 
 export const ActivityControlFlowComponent: SFC<Props> = ({ element }) => {
-  const layoutText = (location: Port['direction']) => {
-    switch (location) {
-      case Direction.Up:
+  let position = { x: 0, y: 0 };
+  let direction: 'v' | 'h' = 'v';
+  const path = element.path.map(point => new Point(point.x, point.y));
+  let distance =
+    path.reduce((length, point, i, points) => (i + 1 < points.length ? length + points[i + 1].subtract(point).length : length), 0) / 2;
+
+  for (let index = 0; index < path.length - 1; index++) {
+    const vector = path[index + 1].subtract(path[index]);
+    if (vector.length > distance) {
+      const norm = vector.normalize();
+      direction = Math.abs(norm.x) > Math.abs(norm.y) ? 'h' : 'v';
+      position = path[index].add(norm.scale(distance));
+      break;
+    }
+    distance -= vector.length;
+  }
+
+  const layoutText = (dir: 'v' | 'h') => {
+    switch (dir) {
+      case 'v':
         return {
-          dx: -5,
-          dominantBaseline: 'text-after-edge',
-          textAnchor: 'end',
-        };
-      case Direction.Right:
-        return {
-          dy: -10,
-          dominantBaseline: 'text-after-edge',
+          dx: 5,
+          dominantBaseline: 'middle',
           textAnchor: 'start',
         };
-      case Direction.Down:
+      case 'h':
         return {
-          dx: -5,
-          dominantBaseline: 'text-before-edge',
-          textAnchor: 'end',
-        };
-      case Direction.Left:
-        return {
-          dy: -10,
+          dy: -5,
           dominantBaseline: 'text-after-edge',
-          textAnchor: 'end',
+          textAnchor: 'middle',
         };
     }
   };
-
-  const source = { ...element.path[0] };
 
   return (
     <g>
@@ -57,7 +59,7 @@ export const ActivityControlFlowComponent: SFC<Props> = ({ element }) => {
         strokeWidth={1}
         markerEnd={`url(#marker-${element.id})`}
       />
-      <text x={source.x} y={source.y} {...layoutText(element.source.direction)}>
+      <text x={position.x} y={position.y} {...layoutText(direction)}>
         {element.name}
       </text>
     </g>
