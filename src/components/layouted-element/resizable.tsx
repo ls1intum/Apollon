6a9fd0381072ee030ee9b7d1c918ec658839cdd7
Julian Willand <willand@in.tum.de>
2019-04-16 15:46:22 +0200

    Refactor resizable to use pointer events. [ARTEMIS-262]

--- src/components/layouted-element/resizable.tsx
+++ src/components/layouted-element/resizable.tsx
@@ -26,7 +26,7 @@ export const resizable = (WrappedComponent: typeof ElementComponent) => {
       return (
         <WrappedComponent {...this.props}>
           {this.props.children}
-          <Handler x={x - 10} y={y - 10} width={15} height={15} onMouseDown={this.onMouseDown} />
+          <Handler x={x - 10} y={y - 10} width={15} height={15} onPointerDown={this.onMouseDown} />
         </WrappedComponent>
       );
     }
@@ -45,18 +45,18 @@ export const resizable = (WrappedComponent: typeof ElementComponent) => {
       this.props.resize(id, { width, height });
     };
 
-    private onMouseDown = (event: React.MouseEvent) => {
-      if (event.nativeEvent.which !== 1) return;
+    private onMouseDown = (event: React.PointerEvent) => {
+      if (event.nativeEvent.which && event.nativeEvent.which !== 1) return;
 
       const position = this.props.getAbsolutePosition(this.props.element.id);
       const offset = position.add(this.props.coordinateSystem.offset());
 
       this.setState({ resizing: true, offset });
-      document.addEventListener('mousemove', this.onMouseMove);
-      document.addEventListener('mouseup', this.onMouseUp);
+      document.addEventListener('pointermove', this.onMouseMove);
+      document.addEventListener('pointerup', this.onMouseUp);
     };
 
-    private onMouseMove = (event: MouseEvent) => {
+    private onMouseMove = (event: PointerEvent) => {
       const width = event.clientX - this.state.offset.x;
       const height = event.clientY - this.state.offset.y;
       const point = this.props.coordinateSystem.screenToPoint(width, height);
@@ -65,8 +65,8 @@ export const resizable = (WrappedComponent: typeof ElementComponent) => {
 
     private onMouseUp = () => {
       this.setState({ resizing: false, offset: new Point() });
-      document.removeEventListener('mousemove', this.onMouseMove);
-      document.removeEventListener('mouseup', this.onMouseUp);
+      document.removeEventListener('pointermove', this.onMouseMove);
+      document.removeEventListener('pointerup', this.onMouseUp);
       this.props.resized(this.props.element.id);
     };
   }
