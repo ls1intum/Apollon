1cf4b7484bd8d4d923b460680535ae643f2b7576
Julian Willand <willand@in.tum.de>
2019-04-19 15:05:57 +0200

    Bugfix: Fixed resizing issue with use case actors.

--- src/packages/use-case-diagram/use-case-actor/use-case-actor.ts
+++ src/packages/use-case-diagram/use-case-actor/use-case-actor.ts
@@ -1,8 +1,18 @@
 import { UseCaseElementType } from '..';
-import { Element } from '../../../services/element/element';
-import { Boundary } from '../../../utils/geometry/boundary';
+import { Element, IElement } from '../../../services/element/element';
+import { UMLElement } from '../../../typings';
 
 export class UseCaseActor extends Element {
   type = UseCaseElementType.UseCaseActor;
-  bounds: Boundary = { ...this.bounds, width: 90, height: 140 };
+
+  constructor(values?: IElement);
+  constructor(values?: UMLElement);
+  constructor(values?: IElement | UMLElement);
+  constructor(values?: IElement | UMLElement) {
+    super(values);
+
+    if (!values) {
+      Object.assign(this, { bounds: { ...this.bounds, width: 90, height: 140 } });
+    }
+  }
 }
