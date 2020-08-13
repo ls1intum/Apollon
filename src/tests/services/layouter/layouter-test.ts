771766eb6b60ac0bbfc4ad413709edc5773cb5fd
Tobias Priesching <tobias.priesching@gmail.com>
2020-08-13 09:44:13 +0200

    add tests

---
+++ src/tests/services/layouter/layouter-test.ts
@@ -0,0 +1,26 @@
+import { createUMLClassWithAttributeAndMethod, getRealStore } from '../../test-utils/test-utils';
+import { UMLElement } from '../../../main/services/uml-element/uml-element';
+import { UMLClassPackage } from '../../../main/packages/uml-class-diagram/uml-class-package/uml-class-package';
+import { render } from '../../../main/services/layouter/layouter';
+
+describe('test layouting of elements', () => {
+  let elements: UMLElement[] = [];
+  let parent: UMLClassPackage;
+
+  beforeEach(() => {
+    // initialize  objects
+    parent = new UMLClassPackage({ name: 'Parent' });
+    const children = createUMLClassWithAttributeAndMethod();
+    parent.ownedElements = [children[0].id];
+    elements.push(parent, ...children);
+  });
+
+  it('render diagram', () => {
+    const store = getRealStore(
+      {},
+      elements.map((element) => ({ ...element })),
+    );
+    // TODO: meaningful test
+    // render(parent.id);
+  });
+});
