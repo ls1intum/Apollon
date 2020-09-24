319fb9e27c991788af8f0f386c584674e297e887
Tobias Priesching <tobias.priesching@gmail.com>
2020-09-24 12:11:15 +0200

    added supportedRelationships to Component of DeploymentDiagram

--- src/main/packages/uml-deployment-diagram/uml-deployment-component/uml-component.ts
+++ src/main/packages/uml-deployment-diagram/uml-deployment-component/uml-component.ts
@@ -1,6 +1,12 @@
-import { DeploymentElementType } from '..';
+import { DeploymentElementType, DeploymentRelationshipType } from "..";
 import { UMLComponent } from '../../common/uml-component/uml-component';
 
 export class UMLDeploymentComponent extends UMLComponent {
+  static supportedRelationships = [
+    DeploymentRelationshipType.DeploymentAssociation,
+    DeploymentRelationshipType.DeploymentDependency,
+    DeploymentRelationshipType.DeploymentInterfaceRequired,
+    DeploymentRelationshipType.DeploymentInterfaceProvided,
+  ];
   type = DeploymentElementType.DeploymentComponent;
 }
