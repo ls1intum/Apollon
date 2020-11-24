5eddd13ff0f16723ac632a31bcb2e06abbc0dc43
Tobias Priesching <tobias.priesching@gmail.com>
2020-11-24 22:08:21 +0100

    fix type error

--- src/main/components/controls/typography/typography.tsx
+++ src/main/components/controls/typography/typography.tsx
@@ -5,7 +5,7 @@ export const defaultProps = {
   gutter: true,
 };
 
-type Props = typeof defaultProps & HTMLAttributes<HTMLElement>;
+type Props = typeof defaultProps & HTMLAttributes<HTMLParagraphElement>;
 
 export const Header = (props: Props) => <Typography variant="header" as="h1" {...props} />;
 
