9a1b9236c172146383ee48c4638de4ab81ca8a63
Melih Oezbeyli <melih.oezbeyli@tum.de>
2021-01-25 08:52:02 +0100

    Add tests for flowchart elements

---
+++ src/tests/packages/flowchart/flowchart-decision/flowchart-decision-update-test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { UMLElement } from '../../../../main/services/uml-element/uml-element';
+import { getRealStore } from '../../../test-utils/test-utils';
+import { wrappedRender } from '../../../test-utils/render';
+import { fireEvent } from '@testing-library/react';
+import { FlowchartDecision } from '../../../../main/packages/flowchart/flowchart-decision/flowchart-decision';
+import { FlowchartDecisionUpdate } from '../../../../main/packages/flowchart/flowchart-decision/flowchart-decision-update';
+
+describe('test flowchart decision update', () => {
+  let elements: UMLElement[] = [];
+  let decision: FlowchartDecision;
+
+  beforeEach(() => {
+    // initialize  objects
+    decision = new FlowchartDecision({
+      id: 'test-id',
+    });
+    elements.push(decision);
+  });
+
+  it('render', () => {
+    const store = getRealStore(undefined, elements);
+
+    const { baseElement } = wrappedRender(<FlowchartDecisionUpdate element={decision} />, { store });
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('change name', () => {
+    const store = getRealStore(undefined, elements);
+
+    const { getByRole } = wrappedRender(<FlowchartDecisionUpdate element={decision} />, {
+      store: store,
+    });
+    const nameField = getByRole('textbox');
+    const updatedValue = 'Start';
+    fireEvent.change(nameField, { target: { value: updatedValue } });
+
+    const updatedElement = store.getState().elements[decision.id] as FlowchartDecision;
+
+    expect(updatedElement.name).toEqual(updatedValue);
+  });
+
+  it('delete', () => {
+    const store = getRealStore(undefined, elements);
+
+    const { getAllByRole } = wrappedRender(<FlowchartDecisionUpdate element={decision} />, { store: store });
+    const buttons = getAllByRole('button');
+    // delete button
+    buttons[0].click();
+
+    expect(store.getState().elements).not.toContain(decision.id);
+  });
+});
---
+++ src/tests/packages/flowchart/flowchart-terminal/flowchart-terminal-update-test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import { UMLElement } from '../../../../main/services/uml-element/uml-element';
+import { getRealStore } from '../../../test-utils/test-utils';
+import { wrappedRender } from '../../../test-utils/render';
+import { fireEvent } from '@testing-library/react';
+import { FlowchartTerminal } from '../../../../main/packages/flowchart/flowchart-terminal/flowchart-terminal';
+import { FlowchartTerminalUpdate } from '../../../../main/packages/flowchart/flowchart-terminal/flowchart-terminal-update';
+
+describe('test flowchart input output update', () => {
+  let elements: UMLElement[] = [];
+  let terminal: FlowchartTerminal;
+
+  beforeEach(() => {
+    // initialize  objects
+    terminal = new FlowchartTerminal({
+      id: 'test-id',
+    });
+    elements.push(terminal);
+  });
+
+  it('render', () => {
+    const store = getRealStore(undefined, elements);
+
+    const { baseElement } = wrappedRender(<FlowchartTerminalUpdate element={terminal} />, { store });
+    expect(baseElement).toMatchSnapshot();
+  });
+
+  it('change name', () => {
+    const store = getRealStore(undefined, elements);
+
+    const { getByRole } = wrappedRender(<FlowchartTerminalUpdate element={terminal} />, {
+      store: store,
+    });
+    const nameField = getByRole('textbox');
+    const updatedValue = 'Start';
+    fireEvent.change(nameField, { target: { value: updatedValue } });
+
+    const updatedElement = store.getState().elements[terminal.id] as FlowchartTerminal;
+
+    expect(updatedElement.name).toEqual(updatedValue);
+  });
+
+  it('delete', () => {
+    const store = getRealStore(undefined, elements);
+
+    const { getAllByRole } = wrappedRender(<FlowchartTerminalUpdate element={terminal} />, { store: store });
+    const buttons = getAllByRole('button');
+    // delete button
+    buttons[0].click();
+
+    expect(store.getState().elements).not.toContain(terminal.id);
+  });
+});
