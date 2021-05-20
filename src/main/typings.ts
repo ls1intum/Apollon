a1c1b4a5fa38d67599b6c6a7fd222cb1b7426dd8
Nicolas Ruscher <nicolas.ruscher@tum.de>
2021-05-20 17:48:26 +0200

    prettier

--- src/main/typings.ts
+++ src/main/typings.ts
@@ -106,6 +106,7 @@ export type Assessment = {
   elementType: UMLElementType | UMLRelationshipType;
   score: number;
   feedback?: string;
+  dropInfo?: any;
   label?: string;
   labelColor?: string;
 };
