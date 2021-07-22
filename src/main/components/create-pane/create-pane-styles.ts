6e4ecbc37bd9f58c06bace5c0ad2e67d756cae45
Melih Oezbeyli <melih.oezbeyli@tum.de>
2021-07-22 21:36:27 +0200

    Separate color legend from rest of the elements

---
+++ src/main/components/create-pane/create-pane-styles.ts
@@ -0,0 +1,9 @@
+import { styled } from '../theme/styles';
+
+export const Separator = styled.div`
+  width: 100%;
+  text-align: center;
+  margin: 1rem 0;
+  height: 2px;
+  background-color: black;
+`;
