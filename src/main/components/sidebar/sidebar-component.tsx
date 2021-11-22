384df72e096544620a883053ffca3b0f5fc70237
rs-maker <robert.schauer@tum.de>
2021-11-22 16:30:32 +0100

    linting

--- src/main/components/sidebar/sidebar-component.tsx
+++ src/main/components/sidebar/sidebar-component.tsx
@@ -45,6 +45,7 @@ const enhance = compose<ComponentClass<OwnProps>>(
 class SidebarComponent extends Component<Props> {
   render() {
     if (this.props.readonly || this.props.mode === ApollonMode.Assessment) return null;
+
     return (
       <Container scale={this.props.scale} id="modeling-editor-sidebar">
         {this.props.mode === ApollonMode.Exporting && (
