2428c65a4e18be19eb9d91165ef155a6e9346406
Martin Achenbach <martin.achenbach@gmx.de>
2022-03-19 17:59:59 +0100

    chore: build root css variables for example app

--- src/main/components/theme/styles.ts
+++ src/main/components/theme/styles.ts
@@ -1,7 +1,7 @@
 import baseStyled, { css as baseCss, ThemedCssFunction, ThemedStyledInterface } from 'styled-components';
 export { withTheme } from 'styled-components';
 
-export type Styles = typeof apollonLight | typeof apollonDark;
+export type Styles = typeof apollonTheme;
 
 export const styled = baseStyled as ThemedStyledInterface<Styles>;
 export const css = baseCss as ThemedCssFunction<Styles>;
@@ -11,61 +11,19 @@ export type withThemeProps = { theme: Styles };
 export type Color = 'primary' | 'secondary';
 export type Size = 'sm' | 'md' | 'lg';
 
-const apollonLight = {
+const apollonTheme = {
   color: {
-    primary: '#2a8fbd',
-    secondary: '#6c757d',
-    warningYellow: '#ffc800',
-    background: '#ffffff',
-    backgroundVariant: '#e5e5e5',
-    primaryContrast: '#212529',
-    gray: '#e9ecef',
-    grayAccent: '#212529',
-    white: '#fff',
-    gray100: '#f8f9fa',
-    gray200: '#e9ecef',
-    gray300: '#dee2e6',
-    gray400: '#ced4da',
-    gray500: '#adb5bd',
-    gray600: '#6c757d',
-    gray700: '#495057',
-    gray800: '#343a40',
-    gray900: '#212529',
-    black: '#212529',
+    primary: 'var(--primary)',
+    secondary: 'var(--secondary)',
+    warningYellow: 'var(--warning-yellow)',
+    background: 'var(--background)',
+    backgroundVariant: 'var(--background-variant)',
+    primaryContrast: 'var(--primary-contrast)',
+    gray: 'var(--gray)',
+    grayAccent: 'var(--gray-variant)',
   },
   font: {
-    color: '#212529',
-    family: 'Helvetica Neue, Helvetica, Arial, sans-serif',
-    size: 16,
-  },
-  interactive: {
-    normal: 'rgba(0, 220, 0, 0.3)',
-    hovered: 'rgba(0, 220, 0, 0.15)',
-  },
-};
-
-const apollonDark = {
-  color: {
-    primary: '#4269a0',
-    secondary: '#6f85c2',
-    warningYellow: '#ffc800',
-    background: '#1B2121',
-    backgroundVariant: '#9BB5BB',
-    primaryContrast: '#faf8f8',
-    gray: '#343a40',
-    grayAccent: '#6c757d',
-    gray900: '#f8f9fa',
-    gray800: '#e9ecef',
-    gray700: '#dee2e6',
-    gray600: '#ced4da',
-    gray500: '#adb5bd',
-    gray400: '#6c757d',
-    gray300: '#495057',
-    gray200: '#343a40',
-    gray100: '#212529',
-  },
-  font: {
-    color: '#faf8f8',
+    color: 'var(--primary',
     family: 'Helvetica Neue, Helvetica, Arial, sans-serif',
     size: 16,
   },
@@ -77,7 +35,7 @@ const apollonDark = {
 
 export const defaults = (scale: number = 1.0) => {
   return {
-    ...apollonDark,
-    font: { ...apollonDark.font, size: apollonDark.font.size * scale },
+    ...apollonTheme,
+    font: { ...apollonTheme.font, size: apollonTheme.font.size * scale },
   };
 };
