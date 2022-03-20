b973674927a1e12189cfc682563a0478c39ce3de
minrows <minrows95@gmail.com>
2022-03-20 16:33:13 +0100

    [MODIFIED] Reverted scaling factor to 20

--- src/main/components/uml-element/resizable/resizable.tsx
+++ src/main/components/uml-element/resizable/resizable.tsx
@@ -117,8 +117,8 @@ export const resizable =
       }
 
       private resize = (width: number, height: number, resizeFrom: ResizeFrom) => {
-        width = Math.round(width / 5) * 5;
-        height = Math.round(height / 5) * 5;
+        width = Math.round(width / 20) * 20;
+        height = Math.round(height / 20) * 20;
         if (options && options.preventX) width = 0;
         if (options && options.preventY) height = 0;
         if (width === 0 && height === 0) return;
