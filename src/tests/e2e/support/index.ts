3dbf5b3a073079bd58e982f4ad6099af7ba1da9d
Martin Achenbach <martin.achenbach@gmx.de>
2022-03-25 08:47:05 +0100

    chore: cleanup of index.ts in cypress

--- src/tests/e2e/support/index.ts
+++ src/tests/e2e/support/index.ts
@@ -6,10 +6,6 @@ import "cypress-real-events/support";
 declare global {
     namespace Cypress {
       interface Chainable<Subject = any> {
-        /**
-         * Custom command to select DOM element by data-cy attribute.
-         * @example cy.dataCy('greeting')
-         */
          visitAndWait(delay?: number): Chainable<JQuery<HTMLElement>>
          selectModelType(typeToSelect: string): Chainable<JQuery<HTMLElement>>
          dragElementIntoCanvas(typeToSelect: number): Chainable<JQuery<HTMLElement>>
