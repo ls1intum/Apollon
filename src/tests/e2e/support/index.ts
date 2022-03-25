e77b359772ec8599b57fca2ff54a19d2f2f08b2f
Martin Achenbach <martin.achenbach@gmx.de>
2022-03-25 20:38:43 +0100

    chore: prettier

--- src/tests/e2e/support/index.ts
+++ src/tests/e2e/support/index.ts
@@ -1,14 +1,14 @@
 import './commands';
 
 // real-events
-import "cypress-real-events/support";
+import 'cypress-real-events/support';
 
 declare global {
-    namespace Cypress {
-      interface Chainable<Subject = any> {
-         visitAndWait(delay?: number): Chainable<JQuery<HTMLElement>>
-         selectModelType(typeToSelect: string): Chainable<JQuery<HTMLElement>>
-         dragElementIntoCanvas(typeToSelect: number): Chainable<JQuery<HTMLElement>>
-      }
+  namespace Cypress {
+    interface Chainable<Subject = any> {
+      visitAndWait(delay?: number): Chainable<JQuery<HTMLElement>>;
+      selectModelType(typeToSelect: string): Chainable<JQuery<HTMLElement>>;
+      dragElementIntoCanvas(typeToSelect: number): Chainable<JQuery<HTMLElement>>;
     }
+  }
 }
