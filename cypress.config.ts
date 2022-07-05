9b666cceb07452e10432ac5ec50af02b542c562a
minrows <minrows95@gmail.com>
2022-07-05 12:10:16 +0200

    [MODIFIED] Migrate to cypress 10

---
+++ cypress.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  numTestsKeptInMemory: 1,
+  viewportWidth: 1800,
+  viewportHeight: 920,
+  video: false,
+  screenshotOnRunFailure: false,
+  fixturesFolder: './src/tests/e2e/fixture',
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:8888',
+    specPattern: './src/tests/e2e/specs/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: './src/tests/e2e/support/index.ts',
+  },
+})
