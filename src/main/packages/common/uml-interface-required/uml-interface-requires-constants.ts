ec556fac3417a16b3a692793255e029b3c176d12
minrows <minrows95@gmail.com>
2022-10-31 12:28:55 +0100

    [MODIFIED] Executed prettier

--- src/main/packages/common/uml-interface-required/uml-interface-requires-constants.ts
+++ src/main/packages/common/uml-interface-required/uml-interface-requires-constants.ts
@@ -1,9 +1,7 @@
 export const REQUIRED_INTERFACE_MARKER_SIZE = 27;
 
 export const enum REQUIRED_INTERFACE_MARKER_TYPE {
-    Semicircle = `M 13 -13.5 a 1 1 0 0 0 0 27`,
-    Threequarterscircle = `M 8 -12.5 C -3.5 -7.5 -3.3 7.9 8 12.5`,
-    Quartercircle = `M 2 -7.8 C -1.5 -3 -1.2 3.4 2 7.8`,
+  Semicircle = `M 13 -13.5 a 1 1 0 0 0 0 27`,
+  Threequarterscircle = `M 8 -12.5 C -3.5 -7.5 -3.3 7.9 8 12.5`,
+  Quartercircle = `M 2 -7.8 C -1.5 -3 -1.2 3.4 2 7.8`,
 }
-
-
