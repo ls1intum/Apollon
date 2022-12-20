4ef7aba03c3012f4e45165781a40bef5a8b0ed62
mertyldrr <mertyildirir14@gmail.com>
2022-12-21 00:31:21 +0100

    [MODIFIED] flowchart input output - adjust coefficents to produce multiple of ten

--- src/main/packages/flowchart/flowchart-input-output/flowchart-input-output-component.tsx
+++ src/main/packages/flowchart/flowchart-input-output/flowchart-input-output-component.tsx
@@ -3,17 +3,19 @@ import { FlowchartComponent } from '../flowchart-element/flowchart-component';
 import { FlowchartInputOutput } from './flowchart-input-output';
 import { ThemedPolyline } from '../../../components/theme/themedComponents';
 
-export const FlowchartInputOutputComponent: FunctionComponent<Props> = ({ element, scale, fillColor }) => (
-  <FlowchartComponent element={element} scale={scale}>
-    <ThemedPolyline
-      points={`${1.1 * element.bounds.width} 0, ${0.9 * element.bounds.width} ${element.bounds.height}, ${
-        -0.1 * element.bounds.width
-      } ${element.bounds.height}, ${0.1 * element.bounds.width} 0, ${1.1 * element.bounds.width} 0`}
-      strokeColor={element.strokeColor}
-      fillColor={fillColor || element.fillColor}
-    />
-  </FlowchartComponent>
-);
+export const FlowchartInputOutputComponent: FunctionComponent<Props> = ({ element, scale, fillColor }) => {
+  return (
+    <FlowchartComponent element={element} scale={scale}>
+      <ThemedPolyline
+        points={`${(13 / 12) * element.bounds.width} 0, ${(11 / 12) * element.bounds.width} ${element.bounds.height}, ${
+          -(1 / 12) * element.bounds.width
+        } ${element.bounds.height}, ${(1 / 12) * element.bounds.width} 0, ${(13 / 12) * element.bounds.width} 0`}
+        strokeColor={element.strokeColor}
+        fillColor={fillColor || element.fillColor}
+      />
+    </FlowchartComponent>
+  );
+};
 
 export interface Props {
   element: FlowchartInputOutput;
