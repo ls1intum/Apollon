79eac4647a720fc09fcd20268a4167696011b562
Stephan Krusche <krusche@in.tum.de>
2023-07-15 09:12:04 +0200

    update dependencies

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,7 +3,6 @@ const common = require('./webpack.common.js');
 
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
-
 module.exports = merge(common, {
   mode: 'production',
   devtool: false,
