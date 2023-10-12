4160f0f8605055cf90632cc23857544ee55a8a94
Matthias Lehner <ga59bip@tum.de>
2023-10-12 14:00:13 +0900

    Move zoom pane styles to styled component to ensure no issues when interoperating with Apollon Standalone

--- src/main/components/canvas/zoom-pane.tsx
+++ src/main/components/canvas/zoom-pane.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from 'react';
 import { clamp } from '../../utils/clamp';
+import { styled } from '../theme/styles';
 
 type Props = {
   min?: number;
@@ -9,21 +10,39 @@ type Props = {
   onChange: (value: number) => void;
 };
 
+const ZoomButton = styled.button`
+  background: var(--apollon-background);
+  color: var(--apollon-primary-contrast);
+  border: 1px solid var(--apollon-gray);
+  margin: 0;
+  outline: none;
+  border-radius: 0.25rem;
+  width: 2.25em;
+  height: 2.25em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  :hover {
+    background-color: var(--apollon-gray);
+    border-color: var(--apollon-gray-variant);
+  }
+
+  :active {
+    background-color: var(--apollon-gray);
+    border-color: var(--apollon-gray-variant);
+  }
+`;
+
 export const ZoomPaneComponent: FunctionComponent<Props> = (props) => {
   const { min = 0.5, max = 5, step = 0.5, value, onChange } = props;
 
   return (
     <div style={{ position: 'absolute', left: '0.75em', bottom: '0.75em', display: 'flex' }}>
-      <button
-        style={{ marginRight: '0.5em' }}
-        className="button-rounded"
-        onClick={() => onChange(clamp(value + step, min, max))}
-      >
+      <ZoomButton style={{ marginRight: '0.5em' }} onClick={() => onChange(clamp(value + step, min, max))}>
         +
-      </button>
-      <button className="button-rounded" onClick={() => onChange(clamp(value - step, min, max))}>
-        -
-      </button>
+      </ZoomButton>
+      <ZoomButton onClick={() => onChange(clamp(value - step, min, max))}>-</ZoomButton>
     </div>
   );
 };
