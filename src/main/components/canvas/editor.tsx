7a53baec20b4d72d13fda285ad03fd9733db84ed
Matthias Lehner <ga59bip@tum.de>
2023-10-29 12:28:13 +0100

    Adapt editor component style causing issues with Artemis

--- src/main/components/canvas/editor.tsx
+++ src/main/components/canvas/editor.tsx
@@ -124,10 +124,7 @@ class EditorComponent extends Component<Props, State> {
 
     if (this.state.isMobile) {
       return (
-        <div
-          ref={this.zoomContainer}
-          style={{ height: '100%', width: '100%', overflow: scale > 1.0 ? 'auto' : 'hidden' }}
-        >
+        <div ref={this.zoomContainer} style={{ width: '100%', overflow: scale > 1.0 ? 'auto' : 'hidden' }}>
           <StyledEditor ref={this.editor} {...props} onTouchMove={this.customScrolling} scale={scale} />
           <ZoomPane
             value={scale}
