62ec8d2272a8f50b7ec5a0f84b8e81921c263de0
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2023-11-19 22:29:13 +0100

    add support for remote selection

---
+++ src/main/services/uml-element/remote-selectable/remote-selection-repository.ts
@@ -0,0 +1,52 @@
+import { UMLElementSelectorType } from '../../../packages/uml-element-selector-type';
+import {
+  RemoteSelectionActionTypes,
+  RemoteSelectionChangeTypes,
+  RemoteSelectionChange,
+  RemoteSelectionChangeAction,
+  RemoteSelectionPruneSelectorsAction,
+} from './remote-selectable-types';
+
+export const RemoteSelectable = {
+  remoteSelectionChange: (
+    selector: UMLElementSelectorType,
+    changes: RemoteSelectionChange[],
+  ): RemoteSelectionChangeAction => ({
+    type: RemoteSelectionActionTypes.SELECTION_CHANGE,
+    payload: {
+      selector,
+      changes,
+    },
+    undoable: false,
+  }),
+
+  remoteSelect: (selector: UMLElementSelectorType, ids: string[]): RemoteSelectionChangeAction =>
+    RemoteSelectable.remoteSelectionChange(
+      selector,
+      ids.map((id) => ({ type: RemoteSelectionChangeTypes.SELECT, id })),
+    ),
+
+  remoteDeselect: (selector: UMLElementSelectorType, ids: string[]): RemoteSelectionChangeAction =>
+    RemoteSelectable.remoteSelectionChange(
+      selector,
+      ids.map((id) => ({ type: RemoteSelectionChangeTypes.DESELECT, id })),
+    ),
+
+  remoteSelectDeselect: (
+    selector: UMLElementSelectorType,
+    select: string[],
+    deselect: string[],
+  ): RemoteSelectionChangeAction =>
+    RemoteSelectable.remoteSelectionChange(selector, [
+      ...select.map((id) => ({ type: RemoteSelectionChangeTypes.SELECT, id })),
+      ...deselect.map((id) => ({ type: RemoteSelectionChangeTypes.DESELECT, id })),
+    ]),
+
+  pruneRemoteSelectors: (allowedSelectors: UMLElementSelectorType[]): RemoteSelectionPruneSelectorsAction => ({
+    type: RemoteSelectionActionTypes.PRUNE_SELECTORS,
+    payload: {
+      allowedSelectors,
+    },
+    undoable: false,
+  }),
+};
