429810fbef57bfc5848d2bdf95ba4ecb8b727814
Matthias Lehner <ga59bip@tum.de>
2023-12-06 21:40:53 +0100

    Remove no longer need import

--- src/main/packages/bpmn/bpmn-task/bpmn-task.ts
+++ src/main/packages/bpmn/bpmn-task/bpmn-task.ts
@@ -1,7 +1,6 @@
 import { BPMNElementType } from '..';
 import { ILayer } from '../../../services/layouter/layer';
 import { ILayoutable } from '../../../services/layouter/layoutable';
-import { calculateNameBounds } from '../../../utils/name-bounds';
 import { UMLElementType } from '../../uml-element-type';
 import { UMLContainer } from '../../../services/uml-container/uml-container';
 import { DeepPartial } from 'redux';
