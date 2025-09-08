462e7951d3f73b27c06b94e9c772b5d70cfce271
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-09-08 18:11:33 +0200

    revert fix?

--- src/main/utils/debounce.ts
+++ src/main/utils/debounce.ts
@@ -3,12 +3,14 @@ export function debounce<TArgs extends unknown[], TThis>(
   wait: number = 0,
 ) {
   let timeout: number | undefined;
-  return function (this: TThis, ...args: TArgs) {
+  // Allow passing additional, unused arguments without type errors
+  return function (this: TThis, ...args: [...TArgs, ...unknown[]]) {
     const context = this;
     clearTimeout(timeout);
     const delay = wait > 0 ? wait : 300;
     timeout = window.setTimeout(() => {
-      return func.apply(context, args);
+      // Only pass the arguments expected by the wrapped function
+      return func.apply(context, args as unknown as TArgs);
     }, delay);
   };
 }
