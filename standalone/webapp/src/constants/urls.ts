dec6b6c8c0ce7e6b1c3220fd8568c970b2db5bbb
tamang29 <tamangravi01@gmail.com>
2026-05-20 13:17:03 +0200

    chore: remove unused code

--- standalone/webapp/src/constants/urls.ts
+++ standalone/webapp/src/constants/urls.ts
@@ -1,8 +1,3 @@
-import { isPlatform } from "@ionic/react"
-
-export const isCapacitorApp =
-  isPlatform("ios") || isPlatform("android") || isPlatform("capacitor")
-
 export const serverURL = import.meta.env.VITE_SERVER_URL || ""
 
 export const serverWSSUrl =
